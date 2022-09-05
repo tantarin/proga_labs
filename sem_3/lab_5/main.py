1e1c9f87dd2b1737c42ff1d274e34819c794d12b
Tanya Tarinskaya <36923363+tantarin@users.noreply.github.com>
2022-09-05 11:34:40 +0300

    Update main.py

--- sem_3/lab_5/main.py
+++ sem_3/lab_5/main.py
@@ -1,3 +1,6 @@
+import configparser
+import logging
+
 PARAMS = {
         'precision': None,
         'output_type': None,
@@ -15,3 +18,19 @@ def load_params(path="lab_5/params.ini"):
     PARAMS['possible_types'] = config.get("Settings", "possible_types")
     PARAMS['dest'] = config.get("Settings", "dest")
     print(PARAMS)
+
+
+load_params()
+def write_log(*args, action=None, result=None, file='calc-history.log.txt'):
+    f = open(file, mode='a', errors='ignore')
+
+    f.write(f"{action}: {args} = {result} \n")
+    f.close()
+
+
+
+def write_log(action=None, result=None, file='history.txt'):
+    logging.basicConfig(filename=file, level=logging.INFO)
+    logging.Formatter("%(asctime)s;%(levelname)s;%(message)s",
+                                  "%Y-%m-%d %H:%M:%S")
+    logging.info("Informational message")
