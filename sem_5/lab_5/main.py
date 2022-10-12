b751b510aeaaa14dd9c145acb2519e3ed2ab54dd
Tanya Tarinskaya <36923363+tantarin@users.noreply.github.com>
2022-10-13 01:01:33 +0300

    Update main.py

--- sem_5/lab_5/main.py
+++ sem_5/lab_5/main.py
@@ -4,26 +4,36 @@ from bs4 import BeautifulSoup
 
 class Bank:
     def __init__(self):
-        self.result = []
-
-    def get_currencies(self, currencies_ids_lst: list) -> list:
+        self.all_valutes = []
         cur_res_str = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
         soup = BeautifulSoup(cur_res_str.content, 'xml')
-        # print(soup.prettify())
-
-        valutes = soup.find_all('Valute')
-        for _v in valutes:
-            valute_id = _v.get('ID')
+        self.valutes = soup.find_all('Valute')
+        for _v in self.valutes:
             valute = {}
-            if str(valute_id) in currencies_ids_lst:
-                valute_cur_name, valute_cur_val = _v.find('Name').text, _v.find('Value').text
-                valute_charcode = _v.find('CharCode').text
-                valute[valute_charcode] = (valute_cur_name, valute_cur_val)
-                self.result.append(valute)
+            valute_cur_name, valute_cur_val = _v.find('Name').text, _v.find('Value').text
+            valute_charcode = _v.find('CharCode').text
+            valute[valute_charcode] = (valute_cur_name, valute_cur_val)
+            self.all_valutes.append(valute)
+        self.valutes = soup.find_all('Valute')
 
-        return self.result
+    def __del__(self):
+         print('Object destroyed')
+
+    def get_currencies(self) -> list:
+        return self.all_valutes
+
+    def set_valute(self, valute_charcode, valute_cur_name, valute_cur_val):
+        valute = {valute_charcode: (valute_cur_name, valute_cur_val)}
+        self.all_valutes.append(valute)
+
+    def get_valute(self, valute_charcode):
+        for v in self.all_valutes:
+            if list(v.keys())[0] == valute_charcode:
+                return v[valute_charcode]
 
 
 bank = Bank()
-res = bank.get_currencies(['R01035', 'R01335', 'R01700J'])
-print(res)
+res = bank.get_currencies()
+for r in res:
+    print(r)
+print(bank.get_valute('AUD'))
