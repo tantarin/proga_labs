afbe029679afc4767b646a56551bc055f0994bd2
Tanya Tarinskaya <36923363+tantarin@users.noreply.github.com>
2022-10-13 18:13:52 +0300

    Update test_fib.py

--- sem_5/lab_4/test_fib.py
+++ sem_5/lab_4/test_fib.py
@@ -1,20 +1,34 @@
-from main import fib, FibonachiList
+from main import fib, FibonachiList, fib_iter, my_genn
 
 
-def test_fib():
-    assert fib(1) == [0, 1], "Тривиальный случай n = 1, список [0, 1]"
-    assert fib(2) == [0, 1, 1]
+def test_zadanie_1():
+    assert fib(1) == [0, 1, 1], "Тривиальный случай n = 1, список [0, 1]"
+    assert fib(2) == [0, 1, 1, 2]
     assert fib(5) == [0, 1, 1, 2, 3, 5]
+    assert fib(7) == [0, 1, 1, 2, 3, 5]
 
 
-def fib(n):
-    myiter = iter(FibonachiList())
+def test_zadanie_2():
+    lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    test_lst = list()
+    for f in FibonachiList(lst):
+        test_lst.append(f)
+    assert test_lst == [0, 1, 1, 2, 3, 5, 8]
 
-    while (myiter.lst[-1] + myiter.lst[-2] <= n):
-        next(myiter)
-    return myiter.lst
+
+def test_zadanie_3():
+    assert list(fib_iter(range(14))) == [0, 1, 1, 2, 3, 5, 8, 13]
+
+
+def test_zadanie_4():
+    test_lst = list()
+    for f in my_genn(7):
+        test_lst.append(f)
+    assert test_lst == [0, 1, 1, 2, 3, 5, 8]
+
 
 if __name__ == "__main__":
-    n = int(input())
-
-    print(fib(n))
+    test_zadanie_1()
+    test_zadanie_2()
+    test_zadanie_3()
+    test_zadanie_4()
