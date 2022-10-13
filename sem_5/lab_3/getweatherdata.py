e0f1adf3bbbbe9fb8e497013ed15eef2e9d6a6cd
Tanya Tarinskaya <36923363+tantarin@users.noreply.github.com>
2022-10-13 21:06:14 +0300

    Create getweatherdata.py

---
+++ sem_5/lab_3/getweatherdata.py
@@ -0,0 +1,28 @@
+import json
+
+import requests
+
+
+def get_weather_data(city_name, api_key=None):
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    data = response.json()
+    result = {}
+
+    if data["cod"] != "404":
+        result["name"] = data["name"]
+        result["coord"] = data["coord"]
+        result["country"] = data["sys"]["country"]
+        result["feels_like"] = data["main"]["feels_like"]
+        hours = int(data["timezone"]) // 3600
+        result["timezone"] = f"UTC+{hours}"
+    print(result)
+    return json.dumps(result)
+
+
+if __name__ == '__main__':
+    owm_api_key = "b11b7be7ea161d31db5ba95ebb0b158e"
+    get_weather_data('Chicago', api_key=owm_api_key)
+    get_weather_data('Saint Petersburg', api_key=owm_api_key)
+    get_weather_data('Dhaka', api_key=owm_api_key)
