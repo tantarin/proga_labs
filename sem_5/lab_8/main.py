8e501f28c3eff1c6233f197e1b2d925712de49c7
Tanya Tarinskaya <36923363+tantarin@users.noreply.github.com>
2022-12-04 14:41:25 +0300

    Create main.py

---
+++ sem_5/lab_8/main.py
@@ -0,0 +1,71 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import parse_qs # распарсить параметры гет и пост-запросов
+
+
+def load_templates():
+    from jinja2 import Environment, PackageLoader, select_autoescape
+    env = Environment(loader=PackageLoader('models', 'templates'),
+                       autoescape=select_autoescape(['html', 'xml']))
+    return env
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        import models
+
+        user1 = models.zhukov.Zhukov()
+        # userDenis = models.ignatev.Ignatev()
+        # userIvan = models.ivan.Ivan()
+        # userIvanova = models.ivanova.Ivanova()
+        # userSergeeva = models.sergeeva.Sergeeva()
+        # userShchegolskiy = models.shchegolskiy.Shchegolskiy()
+        # userEgor = models.sobinin.Sobinin()
+        # userAndrew = models.logwinow.Logwinow()
+        # userMatvey = models.zhuravskiy.Zhuravskiy()
+        # userSmirnov = models.smirnov.Smirnov()
+        # userPanasyuzhenkova = models.panasyuzhenkova.Panasyuzhenkova()
+
+        result = bytes('Пользователи:', 'utf-8')
+        result += bytes(user1.full_name, 'utf-8')
+        # result += bytes(userDenis.full_name, 'utf-8')
+        # result += bytes(userIvan.full_name, 'utf-8')
+        # result += bytes(userIvanova.full_name, 'utf-8')
+        # result += bytes(userSergeeva.full_name, 'utf-8')
+        # result += bytes(userShchegolskiy.full_name, 'utf-8')
+        # result += bytes(userEgor.full_name, 'utf-8')
+         #result += bytes(userAndrew.full_name, 'utf-8')
+        # result += bytes(userMatvey.full_name, 'utf-8')
+        # result += bytes(userSmirnov.full_name, 'utf-8')
+        # result += bytes(userPanasyuzhenkova.full_name, 'utf-8')
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html, charset="utf-8"')
+        self.end_headers()
+
+        list_of_u = user1.full_name
+
+        env = load_templates()
+        template = env.get_template('layout.html')
+
+        result2 = bytes(template.render(list_of_users=list_of_u), 'utf-8')
+
+        self.wfile.write(result2)
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html, charset="utf-8"')
+        self.end_headers()
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        data = parse_qs(str(body, 'utf-8'), keep_blank_values=True)
+        
+        data = {k: v[0] for k, v in data.items() }
+        result = bytes(data['user'], 'utf-8') + bytes(":", 'utf-8') + bytes(data['pass'], 'utf-8')
+        # result = bytes(data)
+        self.wfile.write(result)
+
+
+# Запуск локального сервера
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
