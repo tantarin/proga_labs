f9f85623aa9523f61adbc8df87d15393c8fb4fba
Tanya Tarinskaya <tanya.tarinskaya@gmail.com>
2023-03-26 10:31:21 +0300

    add fields

--- sem_5/lab_10/regform/models.py
+++ sem_5/lab_10/regform/models.py
@@ -1,11 +1,16 @@
-from django.conf import settings
 from django.db import models
-from django.utils import timezone
 
 
 class User(models.Model):
     first_name = models.CharField(max_length=200)
+
+    last_name = models.CharField(max_length=200)
+
     date_of_birth = models.DateField(name='dob')
+
+    password = models.TextField()
+
+    email = models.EmailField(max_length=75)
 
     def __str__(self):
         return self.first_name + ', ' + self.date_of_birth
