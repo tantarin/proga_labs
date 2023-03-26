bdf518abb85d1fe1594d03407f2ddc3528ef6fd7
Tanya Tarinskaya <tanya.tarinskaya@gmail.com>
2023-03-26 10:55:16 +0300

    add new fields

--- sem_5/lab_10/regform/models.py
+++ sem_5/lab_10/regform/models.py
@@ -2,15 +2,17 @@ from django.db import models
 
 
 class User(models.Model):
-    first_name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200, default='Ivan')
 
-    last_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200, default='Ivanov')
+
+    age = models.IntegerField(default=20)
 
     date_of_birth = models.DateField(name='dob')
 
-    password = models.TextField()
+    password = models.CharField(max_length=8, default='123')
 
-    email = models.EmailField(max_length=75)
+    email = models.EmailField(max_length=75, default='from@example.com', )
 
     def __str__(self):
         return self.first_name + ', ' + self.date_of_birth
