d2365498945d423600b1652fe1afe5c3ede92fd6
Tanya Tarinskaya <tanya.tarinskaya@gmail.com>
2023-03-26 11:09:30 +0300

    add form

--- sem_5/lab_10/regform/views.py
+++ sem_5/lab_10/regform/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from regform.forms import SignUpForm
 
 
 def index(request):
@@ -8,3 +11,24 @@ def index(request):
 
 def about(request):
     return render(request, 'regform/about.html', {})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            # load the profile instance created by the signal
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+
+            # login user after signing up
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+
+            # redirect user to home page
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
