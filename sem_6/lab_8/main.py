75d8e99c2390b3b8080f6c9e471e9b35cd94f470
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2023-09-23 01:43:23 +0300

    edit

--- sem_6/lab_8/main.py
+++ sem_6/lab_8/main.py
@@ -1,51 +1,50 @@
+import os
+
 import cv2
 
-facePhoto = "opencv_face_detector.pbtxt"
-faceModel = "opencv_face_detector_uint8.pb"
-faceNet = cv2.dnn.readNet(faceModel, facePhoto)
+
+def loadFaceRecognitionModel():
+    facePhoto = "opencv_face_detector.pbtxt"
+    faceModel = "opencv_face_detector_uint8.pb"
+    return cv2.dnn.readNet(faceModel, facePhoto)
+
+
+def processVideoOrImage(faceNet, option):
+    if option == "1":
+        video = cv2.VideoCapture(0)
+    elif option == "2":
+        image_name = input("Введите имя изображения (например, 'name.jpg'):\n")
+        image_path = "pictures/" + image_name
+        if not os.path.exists(image_path) or not os.path.isfile(image_path):
+            print(f"Файл '{image_path}' не существует или не доступен для чтения.")
+            return
+        video = cv2.VideoCapture(image_path)
+    else:
+        print("Выбрана неверная опция")
+        return
+
+    while cv2.waitKey(1) < 0:
+        hasFrame, frame = video.read()
+        if not hasFrame:
+            cv2.waitKey()
+            break
+
+        resultImg, faceBoxes = highlightFace(faceNet, frame)
+
+        if not faceBoxes:
+            print('Лица не распознаны')
+
+        cv2.imshow("Распознавание лиц", resultImg)
+
 
 def main():
+    faceNet = loadFaceRecognitionModel()
+
     while True:
-        ans = (input("'1' - Video Finder\n'2' - Picture Finder\n"))
-        if ans == "1":
-            video_finder()
+        option = input("'1': Видео\n'2': Изображение\n'q': Выход\n")
+        if option == "q":
             break
-        else:
-            image = (input("Which image? (for example: '1.jpg')\n"))
-            cv2.imshow('New Image', picture_finder(faceNet, image))
-            cv2.waitKey(0)
-            break
-
-
-def picture_finder(net, image, conf_threshold=0.7):
-    imageOpencvDnn = cv2.imread("faces/" + image)
-
-    imageHeight = imageOpencvDnn.shape[0]
-    imageWidth = imageOpencvDnn.shape[1]
-
-    blob = cv2.dnn.blobFromImage(imageOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
-
-    net.setInput(blob) 
-
-    detections = net.forward()
-
-    faceBoxes = []
-
-    for _i in range(detections.shape[2]):
-        confidence = detections[0, 0, _i, 2]
-        if confidence > conf_threshold:
-            x1 = int(detections[0, 0, _i, 3] * imageWidth)
-            y1 = int(detections[0, 0, _i, 4] * imageHeight)
-
-            x2 = int(detections[0, 0, _i, 5] * imageWidth)
-            y2 = int(detections[0, 0, _i, 6] * imageHeight)
-
-            faceBoxes.append([x1, y1, x2, y2])
-
-            cv2.rectangle(imageOpencvDnn, (x1, y1), (x2, y2), (0, 225, 0), int(round(imageHeight/150)), 8)
-
-    cv2.imwrite("faces/" + image[:-4] + "_face_found.jpg", imageOpencvDnn)
-    return imageOpencvDnn
+        processVideoOrImage(faceNet, option)
 
 
 def highlightFace(net, frame, conf_threshold=0.7):
@@ -55,13 +54,9 @@ def highlightFace(net, frame, conf_threshold=0.7):
     frameWidth = frameOpencvDnn.shape[1]
 
     blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
-
     net.setInput(blob)
-
     detections = net.forward()
-
     faceBoxes = []
-
     for _i in range(detections.shape[2]):
         confidence = detections[0, 0, _i, 2]
         if confidence > conf_threshold:
@@ -75,26 +70,7 @@ def highlightFace(net, frame, conf_threshold=0.7):
 
             cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 225, 0), int(round(frameHeight/150)), 8)
 
-        return frameOpencvDnn, faceBoxes 
-
-
-def video_finder():
-    video = cv2.VideoCapture(0)
-
-    while cv2.waitKey(1) < 0:
-        hasFrame, frame = video.read()
-
-        if not hasFrame:
-            cv2.waitKey()
-            break
-
-        resultImg, faceBoxes = highlightFace(faceNet, frame)
-
-        if not faceBoxes:
-            print('Лица не распознаны')
-
-
-        cv2.imshow("Face recognition", resultImg)
+        return frameOpencvDnn, faceBoxes
 
 
 if __name__ == "__main__":
