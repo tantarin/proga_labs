e0bd2210ca9d5ef660259b629843399185f219bc
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2023-10-01 19:44:38 +0300

    delete comments

--- sem_7/lab_1/threading_integrate.py
+++ sem_7/lab_1/threading_integrate.py
@@ -3,28 +3,27 @@ import timeit
 import threading
 
 
+def integrate(f, a, b, n):
+    h = (b - a) / n
+    result = 0.5 * (f(a) + f(b))
+
+    for i in range(1, n):
+        x_i = a + i * h
+        result += f(x_i)
+
+    result *= h
+    return result
+
+
 class Integrator:
     def __init__(self):
         self.lock = threading.Lock()
         self.result = 0.0
 
-    # Функция для численного интегрирования
-    def integrate(self, f, a, b, n):
-        h = (b - a) / n
-        result = 0.5 * (f(a) + f(b))
-
-        for i in range(1, n):
-            x_i = a + i * h
-            result += f(x_i)
-
-        result *= h
-        return result
-
-    # Функция для интегрирования на подинтервале
     def integrate_subinterval(self, f, a, b, n):
         self.lock.acquire()
         try:
-            partial_result = self.integrate(f, a, b, n)
+            partial_result = integrate(f, a, b, n)
             self.result += partial_result
         finally:
             self.lock.release()
@@ -33,13 +32,11 @@ class Integrator:
 def main():
     a = 0
     b = 1
-    n = 1000  # Количество интервалов
-    num_threads = 4  # Количество потоков
-
-    # Создание экземпляра класса Integrator
-    integrator = Integrator()
-
+    n = 1000
+    num_threads = 4
     threads = []
+
+    integrator = Integrator()
 
     for i in range(num_threads):
         start = a + i * (b - a) / num_threads
@@ -48,20 +45,16 @@ def main():
                                   args=(math.sin, start, end, n // num_threads))
         threads.append(thread)
 
-    # Запуск потоков
     for thread in threads:
         thread.start()
 
-    # Ожидание завершения всех потоков
     for thread in threads:
         thread.join()
 
-    # Итоговый результат
     final_result = integrator.result
     print("Интеграл:", final_result)
 
 
 if __name__ == "__main__":
-    # Замер времени выполнения
     execution_time = timeit.timeit(main, number=1)
     print("Время выполнения:", execution_time, "секунд")
