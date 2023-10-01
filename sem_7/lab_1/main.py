c06c11726faf4dc90ab7d38a1661fdba0fc2d7fb
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2023-10-01 11:16:24 +0300

    sem 7

--- sem_7/lab_1/main.py
+++ sem_7/lab_1/main.py
@@ -1,18 +1,32 @@
 import math
 
 
+def rectangle_method(f, a, b, n, mode='left'):
+    h = (b - a) / n
+    points = [a + i * h for i in range(n + 1)]
+    if mode == 'left':
+        values = [f(points[i]) for i in range(n)]
+    else:
+        values = [f(points[i + 1]) for i in range(n)]
+    return h * sum(values)
+
+
+# метод трапеций
 # а и b - диапазон интегрирования
 # f - функция (например, sin, cos, tan, ...) # может быть любая функция из библиотеки math
 # n_iter - число разбиений
-def integrate(f, a, b, n_iter=1000):
-    dx = (b - a) / n_iter
-    area = 0
-    x = a
-    for i in range(n_iter):
-        area += dx * (f(x) + f(x + dx)) / 2
-        x += dx
-    return area
+def integrate_trapezoidal(f, a, b, n):
+    h = (b - a) / n  # Шаг интегрирования
+    result = 0.5 * (f(a) + f(b))  # Первое и последнее значение функции
+
+    for i in range(1, n):
+        x_i = a + i * h
+        result += f(x_i)
+
+    result *= h  # Умножаем на шаг интегрирования
+    return result
 
 
 if __name__ == '__main__':
-    print(integrate(math.sin, 0, 1, n_iter=100))
+    print(integrate_trapezoidal(math.sin, 0, 1, n=100))
+    print(rectangle_method(math.sin, 0, 1, 100, mode='left'))
