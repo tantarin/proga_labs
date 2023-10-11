7c5009c771b5b69783fb07d1265503cfbc853c24
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2023-10-11 23:56:21 +0300

    lab 2

---
+++ sem_7/lab_2/1_3.py
@@ -0,0 +1,31 @@
+import requests
+import concurrent.futures
+
+def make_request(url):
+    response = requests.get(url)
+    return response.text
+
+def main():
+    urls = [
+        "https://docs.python.org/3/",
+        "https://habr.com",
+        "https://tproger.ru"
+    ]
+
+    results = []
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = {executor.submit(make_request, url): url for url in urls}
+
+        for future in concurrent.futures.as_completed(futures):
+            url = futures[future]
+            try:
+                response = future.result()
+                print(f"Запрос к {url} завершен.")
+                results.append(response)
+            except Exception as e:
+                print(f"Запрос к {url} завершился с ошибкой: {str(e)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ sem_7/lab_2/1_4.py
@@ -0,0 +1,19 @@
+import concurrent.futures
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+def main():
+    n = int(input("Введите число для вычисления факториала: "))
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(factorial, n)
+        print("Вычисление факториала...")
+        result = future.result()
+
+    print(f"Факториал {n} равен {result}")
+
+if __name__ == "__main__":
+    main()
