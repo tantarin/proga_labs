42f7dbb3eea10914ba14742533c1fffa51716c47
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2023-11-14 00:52:05 +0300

    add new app

---
+++ sem_7/lab_microservices/mysite/analytics/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+import pandas as pd
+from plotly.offline import plot
+import plotly.express as px
+
+from sem_7.lab_microservices.mysite.analytics.models import Chart
+
+
+def index(request):
+    qs = Chart.objects.all()
+    projects_data = [
+        {
+            'Project': x.name,
+            'Start': x.start_date,
+            'Finish': x.finish_date,
+            'Responsible': x.responsible.username
+        } for x in qs
+    ]
+    df = pd.DataFrame(projects_data)
+    fig = px.timeline(
+        df, x_start="Start", x_end="Finish", y="Project", color="Responsible"
+    )
+    fig.update_yaxes(autorange="reversed")
+    gantt_plot = plot(fig, output_type="div")
+    context = {'plot_div': gantt_plot}
+    return render(request, 'index.html', context)
