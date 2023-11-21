2b2d6034d723485ea94cc9abf0d85d6ebf239095
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2023-11-21 21:45:18 +0300

    add

---
+++ sem_7/lab_3/1.6.py
@@ -0,0 +1,45 @@
+# Улучшите асинхронный веб­скрапер из предыдущей задачи. Преобразуйте его в класс асинхронного менеджера контекста и используйте вместе с async with изнутри асинхронной функции main, которая
+# вызывается через asyncio.run(main()).
+
+import aiohttp
+import asyncio
+from bs4 import BeautifulSoup
+
+class AsyncWebScraper:
+    def __init__(self, urls_file):
+        self.urls_file = urls_file
+        self.urls = self.load_urls()
+
+    def load_urls(self):
+        with open(self.urls_file, 'r') as file:
+            return [line.strip() for line in file]
+
+    async def fetch(self, session, url):
+        async with session.get(url) as response:
+            return await response.text()
+
+    async def scrape_page(self, session, url):
+        html = await self.fetch(session, url)
+        soup = BeautifulSoup(html, 'html.parser')
+        # Здесь вы можете добавить код для извлечения нужной информации с веб-страницы
+        # Например, если вам нужны заголовки:
+        title = soup.title.string
+        print(f"Title from {url}: {title}")
+
+    async def __aenter__(self):
+        self.session = aiohttp.ClientSession()
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self.session.close()
+
+    async def scrape_all(self):
+        tasks = [self.scrape_page(self.session, url) for url in self.urls]
+        await asyncio.gather(*tasks)
+
+async def main():
+    async with AsyncWebScraper("urls.txt") as scraper:
+        await scraper.scrape_all()
+
+if __name__ == "__main__":
+    asyncio.run(main())
